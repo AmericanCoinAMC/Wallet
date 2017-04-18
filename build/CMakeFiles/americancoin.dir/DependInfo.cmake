d360bd97a2dfb61cfac24e2dce7b0df1666a9e10
JessDotJS <jessmanny19@gmail.com>
2017-04-18 15:56:03 -0400

    Media

---
+++ build/CMakeFiles/americancoin.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/americancoin_automoc.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/americancoin_automoc.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/qrc_resources.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/qrc_resources.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/CommandLineParser.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/CommandLineParser.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/CryptoNoteWrapper.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/CryptoNoteWrapper.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/CurrencyAdapter.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/CurrencyAdapter.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/LoggerAdapter.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/LoggerAdapter.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/NodeAdapter.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/NodeAdapter.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/Settings.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/Settings.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/SignalHandler.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/SignalHandler.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/WalletAdapter.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/WalletAdapter.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/AboutDialog.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/AboutDialog.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/AddressBookDialog.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/AddressBookDialog.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/AddressBookFrame.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/AddressBookFrame.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/AddressBookModel.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/AddressBookModel.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/AnimatedLabel.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/AnimatedLabel.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/ChangePasswordDialog.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/ChangePasswordDialog.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/ExitWidget.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/ExitWidget.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/MainWindow.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/MainWindow.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/NewAddressDialog.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/NewAddressDialog.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/NewPasswordDialog.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/NewPasswordDialog.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/OverviewFrame.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/OverviewFrame.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/PasswordDialog.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/PasswordDialog.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/ReceiveFrame.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/ReceiveFrame.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/RecentTransactionsModel.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/RecentTransactionsModel.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/SendFrame.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/SendFrame.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/SortedTransactionsModel.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/SortedTransactionsModel.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/TransactionDetailsDialog.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/TransactionDetailsDialog.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/TransactionFrame.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/TransactionFrame.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/TransactionsFrame.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/TransactionsFrame.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/TransactionsListModel.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/TransactionsListModel.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/TransactionsModel.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/TransactionsModel.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/gui/TransferFrame.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/gui/TransferFrame.cpp.o"
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/src/main.cpp" "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/americancoin.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "GIT_REVISION=\"33ad3f0\""
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  "_GNU_SOURCE"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jessdotjs/Projects/AmericanCoin/Wallet/build/CMakeFiles/cryptonote.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../src"
+  "../cryptonote/external"
+  "../cryptonote/include"
+  "../cryptonote/src"
+  "../cryptonote/src/Platform/Linux"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/americancoin_automoc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "americancoin_automoc.cpp"
+  "cryptonote_automoc.cpp"
+  "CMakeFiles/americancoin_automoc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/americancoin_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
